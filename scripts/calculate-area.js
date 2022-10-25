29e79beace9c57fa843adcfbe6089a495c514f8c
Programmer-Sourav <appsbucket333@gmail.com>
2022-10-25 17:59:03 +0530

    initial commit
    For Mark12 - Fun with Triangles

---
+++ scripts/calculate-area.js
@@ -0,0 +1,18 @@
+const baseSide = document.querySelector("#base-side");
+const heightSide = document.querySelector("#height-side");
+const calculate = document.querySelector("#calculate-area");
+const output = document.querySelector("#output");
+
+function calculateArea(e) {
+  e.preventDefault();
+
+  const baseSideValue = Number(baseSide.value);
+  const heightSideValue = Number(heightSide.value);
+
+
+    const resultArea = (base * height) / 2;
+    return area.toFixed(2);
+    output.innerText = `Area of a triangle using heron's formula is ${resultArea} units`;
+  }
+
+calculate.addEventListener("submit", calculateArea);
